bc087ec40b510ca846fd621d47f9cc0ec6ad61b1
Jay Nagpaul <jaynagpaul@gmail.com>
2018-02-16 22:12:01 -0500

    first commit

---
+++ src/vainglory.cr
@@ -0,0 +1,6 @@
+require "./vainglory/*"
+
+# TODO: Write documentation for `Vainglory`
+module Vainglory
+  # TODO: Put your code here
+end
---
+++ src/vainglory/version.cr
@@ -0,0 +1,3 @@
+module Vainglory
+  VERSION = "0.1.0"
+end
